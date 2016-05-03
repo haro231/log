d360f0bdca600a5e0195aef6caeb516a641a9fc8
Haro231 <1419172@rgu.ac.uk>
2016-05-03 23:17:46 +0100

    data

--- home.php
+++ home.php
@@ -5,3 +5,14 @@
  * Date: 03/05/2016
  * Time: 21:34
  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Practice</title>
+</head>
+<body>
+<h1>HELLO</h1>
+</body>
+</html>
