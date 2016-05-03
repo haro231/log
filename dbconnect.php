e17a44a0b32ff966f10a915d9b5206f9a12f26b2
Haro231 <1419172@rgu.ac.uk>
2016-05-03 22:54:54 +0100

    data

---
+++ dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
+define('DB_USERNAME', 'b4cfc5bf348c66');
+define('DB_PASSWORD', '');
+define('DB_DATABASE','da012a82');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
