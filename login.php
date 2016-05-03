4b4f48347970abc390a8c56bf7315b19e3a68d19
Haro231 <1419172@rgu.ac.uk>
2016-05-04 00:45:02 +0100

    data

--- login.php
+++ login.php
@@ -7,26 +7,23 @@
  */
 
 include("dbconnect.php");
-if(empty($_POST['username'])|| empty($_POST['password'])){
+if(empty($_POST['username']) || empty($_POST['password'])){
     echo "Both fields are required";
 }
-else
-{
+else {
 
-}
+    $username = $_POST ['username'];
+    $password = $_POST ['password'];
 
-$username = $_POST ['username'];
-$password = $_POST ['password'];
+    $sql = "SELECT uid FROM users WHERE username ='$username' and password = '$password'";
 
-$sql = "SELECT uid FROM users WHERE username ='$username' and password = '$password'";
+    $result = mysqli_query($db, $sql);
 
-$result = mysqli_query($db,$sql);
-
-if(mysqli_num_rows($result) == 1){
-    header("location: home.php");
-}
-else{
-    echo "Incorrect username or password" . mysqli_error($result);
+    if (mysqli_num_rows($result) == 1) {
+        header("location: home.php");
+    } else {
+        echo "Incorrect username or password" . mysqli_error($result);
+    }
 }
 ?>
 
