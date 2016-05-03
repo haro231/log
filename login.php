e9c839f98e8d93a06069d2c69ff8503975ebb2b6
Haro231 <1419172@rgu.ac.uk>
2016-05-04 00:36:50 +0100

    data

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
  */
 
 include("dbconnect.php");
-if(empty($_POST["username"])|| empty($_POST["password"])){
+if(empty($_POST['username'])|| empty($_POST['password'])){
     echo "Both fields are required";
 }
 else
