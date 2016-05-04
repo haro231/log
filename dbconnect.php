c589980e78e4240f2cfcaa4fab7846ebff112924
Haro231 <1419172@rgu.ac.uk>
2016-05-04 01:11:19 +0100

    data

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,11 @@
 <?php
 define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
 define('DB_USERNAME', 'b4cfc5bf348c66');
-define('DB_PASSWORD', '');
-define('DB_DATABASE','da012a82');
+define('DB_PASSWORD', 'da012a82');
+define('DB_DATABASE','acsm_7968ef750daf6a5 ');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+if(!$db){
+    echo "Cant connect to database" . mysqli_error();
+}
 ?>
